67d57b6fa98dc91f745cf86fb8d50ae8722efd20
Johnfx879 <ignchrisjohn31@gmail.com>
2024-07-31 15:47:24 +0700

    migrations

--- database/migrations/2024_07_30_091853_create_blogs_table.php
+++ database/migrations/2024_07_30_091853_create_blogs_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->text('tittle');
+            $table->text('content')->nullable();
+            $table->string('status');
             $table->timestamps();
         });
     }
