46cf35cc478209287925f2ae181eb03e0cdf9855
Johnfx879 <ignchrisjohn31@gmail.com>
2024-07-31 15:47:44 +0700

    blog model

--- app/Models/blog.php
+++ app/Models/blog.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class blog extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'user_id',
+        'tittle',
+        'content',
+        'status',
+    ];
 }
