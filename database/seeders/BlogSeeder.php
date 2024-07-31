b2f0d3eef21775c9150b284a2e299b65f2e293e9
Johnfx879 <ignchrisjohn31@gmail.com>
2024-07-31 15:48:03 +0700

    blog seeder

--- database/seeders/BlogSeeder.php
+++ database/seeders/BlogSeeder.php
@@ -2,16 +2,16 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Blog;
 
-class BlogSeeder extends Seeder
+class ProductSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        Blog::factory()->count(50)->create();
     }
 }
