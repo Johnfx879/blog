c54e4fd287d10859631f37196602410abfcb3ee3
Johnfx879 <ignchrisjohn31@gmail.com>
2024-08-07 11:44:55 +0700

    update request

--- app/Http/Requests/UpdateblogRequest.php
+++ app/Http/Requests/UpdateblogRequest.php
@@ -11,7 +11,7 @@ class UpdateblogRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true; // Change this to `true` to allow the request
     }
 
     /**
@@ -22,7 +22,9 @@ class UpdateblogRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:20',
+            'content' => 'required|string',
+            'status' => 'required|string',
         ];
     }
 }
