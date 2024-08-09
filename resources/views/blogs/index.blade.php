17d40c260530670efc8fc61d751103d9c2d30a3e
Johnfx879 <ignchrisjohn31@gmail.com>
2024-08-09 16:05:12 +0700

    success message

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -7,12 +7,20 @@
         </div>
     </x-slot>
 
+    @if (session('success'))
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-4">
+            <div class="bg-green-500 text-white p-4 rounded-md shadow-md">
+                {{ session('success') }}
+            </div>
+        </div>
+    @endif
+
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-4">
         <a href="{{ route('blog.create') }}"
             class="ml-3 inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:ring ring-blue-200 disabled:opacity-25 transition ease-in-out duration-150">Create
             Post</a>
     </div>
-
+    
     <div class="py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
